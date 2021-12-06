137724602293438de489b5ea63067a1800dcce02
Eric Guerra Ribeiro <eric.guerra.07@gmail.com>
2021-12-06 07:29:21 -0300

    Fix spacing

--- dataset.py
+++ dataset.py
@@ -25,6 +25,7 @@ def get_input(path):
         input[i] =  image
     return input
 
+
 def get_label(path):
     """
     """
@@ -43,6 +44,7 @@ def get_classes():
     """
     """
     return np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
 
 if __name__ == "__main__":
     np.savetxt("MNIST\\train_input.gz", get_input(TRAIN_PATH))
