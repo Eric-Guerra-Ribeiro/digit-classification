04f51a5444cd6a3f92587e5f6a352491b8fdca26
Eric Guerra Ribeiro <eric.guerra.07@gmail.com>
2021-12-06 07:34:41 -0300

    Change main to use exported weights

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ if __name__ == "__main__":
     test_input = np.loadtxt("MNIST\\test_input.gz" , dtype=float) 
     test_label = np.loadtxt("MNIST\\test_label.gz" , dtype=str)
     class_list = np.loadtxt("MNIST\\class_list.gz" , dtype=str)
-    least_squares_classifier = least_squares.LeastSquaresMultiClassClassifier(train_input, train_label, class_list)
+    weights = np.loadtxt("MNIST\\weights.gz", dtype=float)
+    least_squares_classifier = least_squares.LeastSquaresMultiClassClassifier(train_input, train_label, class_list, weights)
     least_squares_classifier.test(test_input, test_label)
     least_squares_classifier.show_confusion_matrix(tag="teste")
-    # least_squares_classifier.test(train_input, train_label)
-    # least_squares_classifier.show_confusion_matrix(tag="train")
-
-
+    least_squares_classifier.test(train_input, train_label)
+    least_squares_classifier.show_confusion_matrix(tag="train")
